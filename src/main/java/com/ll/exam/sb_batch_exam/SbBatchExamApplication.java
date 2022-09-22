99638c4513dc23630355bbb41fea0cfac98e5943
Ju, Minji <alswl0w0@gmail.com>
2022-09-22 09:49:58 +0900

    setting

---
+++ src/main/java/com/ll/exam/sb_batch_exam/SbBatchExamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.sb_batch_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBatchExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbBatchExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/sb_batch_exam/SbBatchExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.sb_batch_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBatchExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
