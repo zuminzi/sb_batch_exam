93dc66717bd82a87e101853a32b2f0cd132118fc
Ju, Minji <alswl0w0@gmail.com>
2022-09-22 10:33:04 +0900

    [FEAT] 스프링부트앱을 배치앱으로 설정하여 관련스키마 생성

--- src/main/java/com/ll/exam/sb_batch_exam/SbBatchExamApplication.java
+++ src/main/java/com/ll/exam/sb_batch_exam/SbBatchExamApplication.java
@@ -1,9 +1,11 @@
 package com.ll.exam.sb_batch_exam;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SbBatchExamApplication {
 
 	public static void main(String[] args) {
