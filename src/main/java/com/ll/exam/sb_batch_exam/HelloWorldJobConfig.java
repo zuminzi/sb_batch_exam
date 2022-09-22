a473fb9da9ce80eb90875e6bbfb5748eafeb598b
Ju, Minji <alswl0w0@gmail.com>
2022-09-22 10:36:15 +0900

    [FEAT] helloWorldJob 추가 후 실행

---
+++ src/main/java/com/ll/exam/sb_batch_exam/HelloWorldJobConfig.java
@@ -0,0 +1,45 @@
+package com.ll.exam.sb_batch_exam;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloWorldJobConfig {
+    private final JobBuilderFactory jobBuilderFactory; // job 생성 x 빌더 o
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloWorldJob() {
+        return jobBuilderFactory.get("helloWorldJob")// Bean으로 찾기 때문에 job 이름은 유니크해야됨
+                .start(helloWorldStep1())
+                .build();
+    }
+
+    @Bean
+    public Step helloWorldStep1() {
+        return stepBuilderFactory.get("helloWorldStep1")
+                .tasklet(helloWorldTasklet())
+                .build();
+    }
+
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return (contribution, chunkContext) -> {
+            System.out.println("헬로월드!");
+
+            return RepeatStatus.FINISHED;
+        };
+    }
+}
