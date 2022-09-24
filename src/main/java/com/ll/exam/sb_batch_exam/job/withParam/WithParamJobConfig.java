01c024af37da5988fcf66d1ec313144d185b1e92
Ju, Minji <alswl0w0@gmail.com>
2022-09-24 22:52:36 +0900

    [FEAT] WithParam Job Parameter 추가

--- src/main/java/com/ll/exam/sb_batch_exam/job/withParam/WithParamJobConfig.java
+++ src/main/java/com/ll/exam/sb_batch_exam/job/withParam/WithParamJobConfig.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,21 +21,28 @@ public class WithParamJobConfig {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job withParamJob() {
+    public Job withParamJob(Step withParamStep1) {
         return jobBuilderFactory.get("withParamJob")
-                .start(withParamStep1())
+                .start(withParamStep1)
                 .build();
     }
-
-    private Step withParamStep1() {
+    @Bean
+    @JobScope
+    public Step withParamStep1(Tasklet withParamStep1Tasklet) {
         return stepBuilderFactory.get("withParamStep1")
-                .tasklet(withParamStep1Tasklet())
+                .tasklet(withParamStep1Tasklet)
                 .build();
     }
 
-    private Tasklet withParamStep1Tasklet() {
+    @Bean
+    @StepScope
+    public Tasklet withParamStep1Tasklet(
+            @Value("#{jobParameters['name']}") String name,
+            @Value("#{jobParameters['age']}") int age
+    ) {
+
         return (contribution, chunkContext) -> {
-            System.out.println("WithParam 테스클릿 1");
+            System.out.println("WithParam 테스클릿 1, %s, %d".formatted(name, age));
 
             return RepeatStatus.FINISHED;
         };
