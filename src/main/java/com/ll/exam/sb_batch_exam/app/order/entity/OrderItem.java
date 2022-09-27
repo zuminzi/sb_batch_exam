03a4e1bfc65d6e7baaffc25fc2346d57404aee82
Ju Minji <alswl0w0@gmail.com>
2022-09-27 09:43:19 +0900

    [FEAT] 통계와 정산을 위해, 주문품목에 그 당시 판매가, 도매가 등을 기록

--- src/main/java/com/ll/exam/sb_batch_exam/app/order/entity/OrderItem.java
+++ src/main/java/com/ll/exam/sb_batch_exam/app/order/entity/OrderItem.java
@@ -28,9 +28,15 @@ public class OrderItem extends BaseEntity {
     private ProductOption productOption;
 
     private int quantity;
+    private int price; // 권장판매가
+    private int salePrice; // 실제판매가
+    private int wholesalePrice; // 도매가
 
     public OrderItem(ProductOption productOption, int quantity) {
         this.productOption = productOption;
         this.quantity = quantity;
+        this.price = productOption.getPrice();
+        this.salePrice = productOption.getSalePrice();
+        this.wholesalePrice = productOption.getWholesalePrice();
     }
 }
