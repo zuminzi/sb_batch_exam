472c87d3281610091d0458a96c5298c801683607
Ju Minji <alswl0w0@gmail.com>
2022-10-29 15:52:29 +0900

    [FEAT] 부분적인 범위 정산

--- src/main/java/com/ll/exam/sb_batch_exam/app/order/repository/OrderItemRepository.java
+++ src/main/java/com/ll/exam/sb_batch_exam/app/order/repository/OrderItemRepository.java
@@ -1,9 +1,13 @@
 package com.ll.exam.sb_batch_exam.app.order.repository;
 
 import com.ll.exam.sb_batch_exam.app.order.entity.Order;
+import com.ll.exam.sb_batch_exam.app.order.entity.OrderItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface OrderItemRepository extends JpaRepository<Order, Long> {
+    Page<OrderItem> findAllByIdLessThan(long id, Pageable pageable);
 }
 
--- src/main/java/com/ll/exam/sb_batch_exam/job/makeRebateOrderItem/MakeRebateOrderItemJobConfig.java
+++ src/main/java/com/ll/exam/sb_batch_exam/job/makeRebateOrderItem/MakeRebateOrderItemJobConfig.java
@@ -74,9 +74,9 @@ public class MakeRebateOrderItemJobConfig {
         return new RepositoryItemReaderBuilder<OrderItem>()
                 .name("orderItemReader")
                 .repository(orderItemRepository)
-                .methodName("findAll")
+                .methodName("findAllByIdLessThan")
                 .pageSize(100)
-                .arguments(Arrays.asList())
+                .arguments(Arrays.asList(6L))
                 .sorts(Collections.singletonMap("id", Sort.Direction.ASC))
                 .build();
     }
