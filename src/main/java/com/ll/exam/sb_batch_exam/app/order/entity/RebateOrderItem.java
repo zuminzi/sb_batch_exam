5f3928e0eb4efa0ec3ecad3f0dec2168fb27dd4f
Ju Minji <alswl0w0@gmail.com>
2022-10-29 17:15:07 +0900

    [REFACTOR] 상품옵션 필드들을 하나의 클래스로 묶어서 정산테이블에는 객체필드로 존재하도록
    *@Embeddable -> 컬럼들 묶을 클래스에 붙이는 어노테이션
    @Embedded -> 클래스 객체 필드에 붙이는 어노테이션

--- src/main/java/com/ll/exam/sb_batch_exam/app/order/entity/RebateOrderItem.java
+++ src/main/java/com/ll/exam/sb_batch_exam/app/order/entity/RebateOrderItem.java
@@ -58,10 +58,14 @@ public class RebateOrderItem extends BaseEntity {
     private String productName;
 
     // 상품옵션
-    private String productOptionColor;
-    private String productOptionSize;
-    private String productOptionDisplayColor;
-    private String productOptionDisplaySize;
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "color", column = @Column(name = "product_option_color")),
+            @AttributeOverride(name = "size", column = @Column(name = "product_option_size")),
+            @AttributeOverride(name = "displayColor", column = @Column(name = "product_option_display_color")),
+            @AttributeOverride(name = "displaySize", column = @Column(name = "product_option_display_size"))
+    })
+    private RebateOrderItem.EmbProductOption embProductOption;
 
     // 주문품목
     private LocalDateTime orderItemCreateDate;
@@ -83,12 +87,25 @@ public class RebateOrderItem extends BaseEntity {
         // 상품 추가 데이터
         productName = orderItem.getProductOption().getProduct().getName();
         // 상품 옵션 추가 데이터
-        productOptionColor = orderItem.getProductOption().getColor();
-        productOptionSize = orderItem.getProductOption().getSize();
-        productOptionDisplayColor = orderItem.getProductOption().getDisplayColor();
-        productOptionDisplaySize = orderItem.getProductOption().getDisplaySize();
+        embProductOption = new EmbProductOption(orderItem.getProductOption());
         // 주문품목 추가 데이터
         orderItemCreateDate = orderItem.getCreateDate();
+    }
+
+    @Embeddable
+    @NoArgsConstructor
+    public static class EmbProductOption {
+        private String color;
+        private String size;
+        private String displayColor;
+        private String displaySize;
+
+        public EmbProductOption(ProductOption productOption) {
+            color = productOption.getColor();
+            size = productOption.getSize();
+            displayColor = productOption.getDisplayColor();
+            displaySize = productOption.getDisplaySize();
+        }
     }
 }
 
